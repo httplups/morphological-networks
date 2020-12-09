59c87f3e8a16445ee255ed4632204efbd89dc78d
Luana Barros <dev.luana@gmail.com>
2020-12-09 17:41:14 -0300

    dssdds

---
+++ libraries/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setupsetup(
+    name='MLMP',
+    packages=find_packages(include=[MLMP]),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner','wheel'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
