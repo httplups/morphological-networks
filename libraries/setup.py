efb16250230b005e363a41f4cd4689a476241a08
Luana Barros <dev.luana@gmail.com>
2020-12-09 17:44:34 -0300

    sdds

--- libraries/setup.py
+++ libraries/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='MLMP',
-    packages=find_packages(include=[MLMP]),
+    packages=find_packages(include=['MLMP']),
     version='0.1.0',
     description='My first Python library',
     author='Me',
